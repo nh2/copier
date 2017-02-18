616967331c4c2e32930aa59a091f368778df629f
Niklas Hambüchen <mail@nh2.me>
2017-02-18 21:43:43 +0100

    Make stat() parallel by using a safe foreign call, add CLI flags

--- src/Copier.hs
+++ src/Copier.hs
@@ -15,22 +15,25 @@ import           Data.Monoid ((<>))
 import           Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
+import           Data.Time.Clock (diffTimeToPicoseconds, utctDayTime)
+import           Data.Time.Clock.POSIX (posixSecondsToUTCTime)
 import           Data.Traversable (for)
 import           GHC.Stack (HasCallStack)
 import           Options.Applicative (Parser, argument, str, option, auto, value, help, metavar, long, short)
 import qualified Options.Applicative as Opts
-import           System.Directory (copyFile, removeDirectoryRecursive)
+import           System.Directory (removeDirectoryRecursive)
 import           System.Exit (die)
 import qualified System.FilePath as FP
 import           System.IO.Error (catchIOError)
 import           System.Posix.Directory.Traversals (allDirectoryContents')
-import           System.Posix.FilePath (RawFilePath, (</>), takeFileName)
-import           System.Posix.Files.ByteString (getFileStatus, getSymbolicLinkStatus, isDirectory, isRegularFile, isSymbolicLink, fileMode, fileExist, directoryMode, createSymbolicLink, readSymbolicLink, removeLink)
-import           System.Posix.Directory.ByteString (createDirectory, removeDirectory)
+import           System.Posix.FilePath (RawFilePath, (</>))
+import           System.Posix.Files.ByteString (isDirectory, isRegularFile, isSymbolicLink, fileMode, fileExist, createSymbolicLink, readSymbolicLink, removeLink, modificationTimeHiRes, accessTimeHiRes, fileSize, setFileTimesHiRes)
+import           System.Posix.Directory.ByteString (createDirectory)
 import           System.Posix.Types (FileMode)
 
 import           PooledMapConcurrently (pooledMapConcurrently')
 import           Sendfile (copyFileSendfile)
+import           SafeFFIStat (getSymbolicLinkStatusNonBlocking)
 
 
 -- | Command line arguments of this program.
@@ -38,6 +41,8 @@ data CLIArgs = CLIArgs
   { sourceDir :: ByteString
   , destDir :: ByteString
   , numJobs :: Int
+  , assumeMicrosecondTimeResolution :: Bool
+  , verbose :: Bool
   } deriving (Eq, Ord, Show)
 
 
@@ -54,6 +59,18 @@ cliArgsParser = CLIArgs
         <> value 1
         <> help "Number of parallel copy jobs"
       )
+  <*> Opts.switch
+      ( long "assume-microsecond-time-resolution"
+        <> help ("Don't try to synchronise mtimes when the difference between"
+                 ++ " source and target mtime is less than 1000 nanoseconds"
+                 ++ " (useful e.g. for versions of GlusterFS that don't support"
+                 ++ " nanosecond time stamps)")
+      )
+  <*> Opts.switch
+      ( long "verbose"
+        <> short 'v'
+        <> help "Increase verbosity"
+      )
 
 
 -- | Parses the command line arguments for this program.
@@ -61,7 +78,7 @@ parseArgs :: IO CLIArgs
 parseArgs = Opts.execParser $
   Opts.info
     (Opts.helper <*> cliArgsParser)
-    (Opts.fullDesc <> Opts.progDesc "Copies files in parallel")
+    (Opts.fullDesc <> Opts.progDesc "Copies files in parallel. Makes DEST_DIR contain at least all contents from SOURCE_DIR.")
 
 
 -- | Decodes a ByteString of which we know it must be UTF-8,
@@ -76,7 +93,10 @@ decodeUtf8OrDie bs = case T.decodeUtf8' bs of
 -- Returns True when it did exist.
 removeIfExists :: RawFilePath -> IO Bool
 removeIfExists path = do
-  mbStat <- (Just <$> getSymbolicLinkStatus path) `catchIOError` \_ -> return Nothing
+  -- TODO: Now this is not pretty with the `catchIOError` to decide
+  -- if the symlink dosn't exist, but that's how `doesFileExist`
+  -- does it. Switch this to a proper `lstat()` ENOENT handling later.
+  mbStat <- (Just <$> getSymbolicLinkStatusNonBlocking path) `catchIOError` \_ -> return Nothing
   case mbStat of
     Nothing -> return False
     Just stat
@@ -88,7 +108,6 @@ removeIfExists path = do
 -- | Creates a directory with given permissions, if it doesn't exist.
 mkdirIfMissing :: RawFilePath -> FileMode -> IO ()
 mkdirIfMissing dir mode = do
-  exists <- fileExist dir
   _ <- removeIfExists dir
   putStrLn $ "creating dir " ++ show dir
   createDirectory dir mode
@@ -105,6 +124,8 @@ main = do
     { sourceDir = sourceDirUnNormalised
     , destDir = destDirUnNormalised
     , numJobs
+    , assumeMicrosecondTimeResolution
+    , verbose
     } <- parseArgs
 
   -- Arguments validation.
@@ -129,29 +150,74 @@ main = do
 
       let copyCreate sourcePath = do
 
+            when verbose $ putStrLn $ "Considering " ++ show sourcePath
             let pathInsideSourceDir = BS.drop prefixLength sourcePath
             let dest = destDir </> pathInsideSourceDir
 
-            sourceStatus <- getSymbolicLinkStatus sourcePath
+            sourceStatus <- getSymbolicLinkStatusNonBlocking sourcePath
             if
               | isDirectory sourceStatus -> do
                   mkdirIfMissing dest (fileMode sourceStatus) -- preserve dir perms
               | isSymbolicLink sourceStatus -> do
                   linkPointerTarget <- readSymbolicLink sourcePath
-                  -- TODO: Now this is not pretty with the `catchIOError` to decide
-                  -- if the symlink dosn't exist, but that's how `doesFileExist`
-                  -- does it. Switch this to a proper `lstat()` ENOENT handling later.
                   _ <- removeIfExists dest
                   createSymbolicLink linkPointerTarget dest
               | isRegularFile sourceStatus -> do
                   let toFileCreateMode = fileMode sourceStatus
-                  copyFileSendfile sourcePath (Just sourceStatus) dest toFileCreateMode
+                  let performFullCopy = copyFileSendfile sourcePath (Just sourceStatus) dest toFileCreateMode
+
+                  -- let sourceAtime = accessTimeHiRes sourceStatus
+                  let sourceMtime = modificationTimeHiRes sourceStatus
+                  let sourceSize = fileSize sourceStatus
+                  m'destStatus <- (Just <$> getSymbolicLinkStatusNonBlocking dest) `catchIOError` \_ -> return Nothing
+
+                  case m'destStatus of
+                    Nothing -> do
+                      when verbose $ putStrLn $ "  Performing copy (nonexistent) " ++ T.unpack (decodeUtf8OrDie sourcePath)
+                      performFullCopy
+
+                    Just destStatus -> do
+                      let destAtime = accessTimeHiRes destStatus
+                      let destMtime = modificationTimeHiRes destStatus
+                      let destSize = fileSize destStatus
+
+                      let mtimeMicrosDiff = sourceMtimeMicros - destMtimeMicros
+                            where
+                              sourceMtimeMicros = diffTimeToPicoseconds (utctDayTime (posixSecondsToUTCTime sourceMtime)) `quot` 1000000
+                              destMtimeMicros = diffTimeToPicoseconds (utctDayTime (posixSecondsToUTCTime destMtime)) `quot` 1000000
+
+                      let mtimesDisagree
+                            | assumeMicrosecondTimeResolution = mtimeMicrosDiff > 0
+                            | otherwise                       = sourceMtime > destMtime
+
+                      if (sourceSize /= destSize) || mtimesDisagree
+                        then do
+                          when verbose $ putStrLn $ "  Performing copy (size differs) " ++ T.unpack (decodeUtf8OrDie sourcePath)
+                          performFullCopy
+                        else do
+                          when verbose $ putStrLn $ "  Skipped copying " ++ T.unpack (decodeUtf8OrDie sourcePath)
+                          return ()
+
+                      when mtimesDisagree $ do
+                        when verbose $ putStrLn $ "  Setting times " ++ T.unpack (decodeUtf8OrDie sourcePath)
+                        -- TODO fix unix package to handle EINTR in futimens();
+                        --      Ubuntu's `man futimens` doesn't document that EINTR can happen
+                        --      but some man pages on the internet suggest it can and I found
+                        --      it to occur as my Ctrl-Z on this program resulted in
+                        --        setFileTimesHiRes: interrupted (Interrupted system call)
+                        --      see https://github.com/haskell/unix/issues/86
+                        setFileTimesHiRes dest destAtime sourceMtime
+
               | otherwise -> do
                   die $ "Can only copy regular files, symlinks and directories, but this isn't one: " ++ T.unpack (decodeUtf8OrDie sourcePath)
+            when verbose $ putStrLn $ "  done considering " ++ show sourcePath
+            return ()
 
       (dirs, files) <- (partitionEithers <$>) $ for sourcePaths $ \sourcePath -> do
-        stat <- getSymbolicLinkStatus sourcePath
+        stat <- getSymbolicLinkStatusNonBlocking sourcePath
         pure $ if isDirectory stat then Left sourcePath else Right sourcePath
+
+      when verbose $ putStrLn $ "Syncing " ++ show (length dirs) ++ " dirs, " ++ show (length files) ++ " files"
 
       for_ dirs copyCreate
       _ <- pooledMapConcurrently' numJobs copyCreate files
